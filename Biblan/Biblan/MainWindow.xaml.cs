06c736a955291db675011dda6ec6fdc316f24c9d
Pontus <pontus.byqvist@gmail.com>
2010-10-26 10:46:44 +0000

    Flyttar button till hörnet.

--- Biblan/Biblan/MainWindow.xaml.cs
+++ Biblan/Biblan/MainWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace Biblan
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
