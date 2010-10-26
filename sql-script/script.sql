e1772811c91f3b93f8552181bc566859ae9acba2
mirontoli <mirontoli@gmail.com>
2010-10-26 12:21:36 +0000

    lagt till procedure och function samt två insert...

--- sql-script/script.sql
+++ sql-script/script.sql
@@ -54,5 +54,30 @@ CREATE TABLE Borrow (
     CONSTRAINT Borrow_Customer_FK FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID)
     );
 
--- Goran test
--- create procedure...
+
+
+
+-- Create functions Anatoly 20101026
+CREATE FUNCTION search_books_titles(@title VARCHAR(75))
+RETURNS TABLE
+AS RETURN (
+	SELECT *
+	FROM Book
+	WHERE Title LIKE '%' + @title + '%'
+)
+
+
+CREATE PROCEDURE usp_add_book
+@isbn VARCHAR(35),
+@title VARCHAR(75),
+@number_of_pages INT,
+@print_year INT,
+@publisher VARCHAR(50)
+AS
+	begin
+		INSERT INTO Book values(@isbn, @title, @number_of_pages, @print_year, @publisher)
+	end
+
+-- inserts Anatoly 20101026
+insert into Book values('9789121100523', 'Latinsk grammatik', 247, 1989, 'Almqvist & Wiksell läromedel');
+exec usp_add_book '9781412929554', 'Global shift, mapping the changing contours of the world economy', 599, 2007, 'Sage'
