3d0571f474e44d71242073ef5eeb11644f43fb2d
mirontoli <mirontoli@gmail.com>
2010-10-28 09:42:58 +0000

    Anpassat lite till MVC. MainWindow har flyttats till Views.

--- Biblan/Biblan/Views/MainWindow.xaml.cs
+++ Biblan/Biblan/Views/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Biblan
+namespace Biblan.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
