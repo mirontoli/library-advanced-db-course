c87b2d732eaa52061a6b15b76e474b465f0c26ad
Sinan <nusotre@gmail.com>
2010-10-28 09:24:31 +0000

    Börjat skriva model klasser (properties)

---
+++ Biblan/Biblan/Model/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Biblan.Model
+{
+    class Book
+    {
+        public string ISBN
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public int NumberOfPages
+        {
+            get;
+            set;
+        }
+
+        public int PrintYear
+        {
+            get;
+            set;
+        }
+
+        public string Publisher
+        {
+            get;
+            set;
+        }
+
+    }
+}
