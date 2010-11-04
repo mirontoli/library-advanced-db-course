e3699e19ce365bac1863a8efb2c4b178d49de29f
Pontus <pontus.byqvist@gmail.com>
2010-11-04 15:14:18 +0000

    Small extras

---
+++ Biblan/Biblan/Views/SmallExtras.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Biblan.Views
+{
+    /// <summary>
+    /// Interaction logic for SmallExtras.xaml
+    /// </summary>
+    public partial class SmallExtras : Window
+    {
+        public SmallExtras()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMoreThanOneBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
