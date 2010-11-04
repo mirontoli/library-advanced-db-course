f0d787dbdd2918b8c646b1a325c9ddc1a63f4cb0
Pontus <pontus.byqvist@gmail.com>
2010-11-04 12:20:08 +0000

    Lagt till Author-kolumnen i de övriga två functions också.

--- Biblan/Biblan/bibliotek.designer.cs
+++ Biblan/Biblan/bibliotek.designer.cs
@@ -83,6 +83,12 @@ namespace Biblan
 			return ((int)(result.ReturnValue));
 		}
 		
+		[global::System.Data.Linq.Mapping.FunctionAttribute(Name="dbo.search_books_titles", IsComposable=true)]
+		public IQueryable<search_books_titlesResult> search_books_titles([global::System.Data.Linq.Mapping.ParameterAttribute(DbType="VarChar(75)")] string title)
+		{
+			return this.CreateMethodCallQuery<search_books_titlesResult>(this, ((MethodInfo)(MethodInfo.GetCurrentMethod())), title);
+		}
+		
 		[global::System.Data.Linq.Mapping.FunctionAttribute(Name="dbo.search_book_print_year", IsComposable=true)]
 		public IQueryable<search_book_print_yearResult> search_book_print_year()
 		{
@@ -94,11 +100,121 @@ namespace Biblan
 		{
 			return this.CreateMethodCallQuery<search_number_of_book_pagesResult>(this, ((MethodInfo)(MethodInfo.GetCurrentMethod())));
 		}
+	}
+	
+	public partial class search_books_titlesResult
+	{
 		
-		[global::System.Data.Linq.Mapping.FunctionAttribute(Name="dbo.search_books_titles", IsComposable=true)]
-		public IQueryable<search_books_titlesResult> search_books_titles([global::System.Data.Linq.Mapping.ParameterAttribute(DbType="VarChar(75)")] string title)
+		private string _ISBN;
+		
+		private string _Title;
+		
+		private System.Nullable<int> _NumberOfPages;
+		
+		private System.Nullable<int> _PrintYear;
+		
+		private string _Publisher;
+		
+		private string _Author;
+		
+		public search_books_titlesResult()
 		{
-			return this.CreateMethodCallQuery<search_books_titlesResult>(this, ((MethodInfo)(MethodInfo.GetCurrentMethod())), title);
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_ISBN", DbType="VarChar(35) NOT NULL", CanBeNull=false)]
+		public string ISBN
+		{
+			get
+			{
+				return this._ISBN;
+			}
+			set
+			{
+				if ((this._ISBN != value))
+				{
+					this._ISBN = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Title", DbType="VarChar(75)")]
+		public string Title
+		{
+			get
+			{
+				return this._Title;
+			}
+			set
+			{
+				if ((this._Title != value))
+				{
+					this._Title = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_NumberOfPages", DbType="Int")]
+		public System.Nullable<int> NumberOfPages
+		{
+			get
+			{
+				return this._NumberOfPages;
+			}
+			set
+			{
+				if ((this._NumberOfPages != value))
+				{
+					this._NumberOfPages = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_PrintYear", DbType="Int")]
+		public System.Nullable<int> PrintYear
+		{
+			get
+			{
+				return this._PrintYear;
+			}
+			set
+			{
+				if ((this._PrintYear != value))
+				{
+					this._PrintYear = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Publisher", DbType="VarChar(50)")]
+		public string Publisher
+		{
+			get
+			{
+				return this._Publisher;
+			}
+			set
+			{
+				if ((this._Publisher != value))
+				{
+					this._Publisher = value;
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Author", DbType="VarChar(50)")]
+		public string Author
+		{
+			get
+			{
+				return this._Author;
+			}
+			set
+			{
+				if ((this._Author != value))
+				{
+					this._Author = value;
+				}
+			}
 		}
 	}
 	
@@ -114,6 +230,8 @@ namespace Biblan
 		private System.Nullable<int> _PrintYear;
 		
 		private string _Publisher;
+		
+		private string _Author;
 		
 		public search_book_print_yearResult()
 		{
@@ -198,6 +316,22 @@ namespace Biblan
 				}
 			}
 		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Author", DbType="VarChar(50)")]
+		public string Author
+		{
+			get
+			{
+				return this._Author;
+			}
+			set
+			{
+				if ((this._Author != value))
+				{
+					this._Author = value;
+				}
+			}
+		}
 	}
 	
 	public partial class search_number_of_book_pagesResult
@@ -213,107 +347,9 @@ namespace Biblan
 		
 		private string _Publisher;
 		
-		public search_number_of_book_pagesResult()
-		{
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_ISBN", DbType="VarChar(35) NOT NULL", CanBeNull=false)]
-		public string ISBN
-		{
-			get
-			{
-				return this._ISBN;
-			}
-			set
-			{
-				if ((this._ISBN != value))
-				{
-					this._ISBN = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Title", DbType="VarChar(75)")]
-		public string Title
-		{
-			get
-			{
-				return this._Title;
-			}
-			set
-			{
-				if ((this._Title != value))
-				{
-					this._Title = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_NumberOfPages", DbType="Int")]
-		public System.Nullable<int> NumberOfPages
-		{
-			get
-			{
-				return this._NumberOfPages;
-			}
-			set
-			{
-				if ((this._NumberOfPages != value))
-				{
-					this._NumberOfPages = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_PrintYear", DbType="Int")]
-		public System.Nullable<int> PrintYear
-		{
-			get
-			{
-				return this._PrintYear;
-			}
-			set
-			{
-				if ((this._PrintYear != value))
-				{
-					this._PrintYear = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Publisher", DbType="VarChar(50)")]
-		public string Publisher
-		{
-			get
-			{
-				return this._Publisher;
-			}
-			set
-			{
-				if ((this._Publisher != value))
-				{
-					this._Publisher = value;
-				}
-			}
-		}
-	}
-	
-	public partial class search_books_titlesResult
-	{
-		
-		private string _ISBN;
-		
-		private string _Title;
-		
-		private System.Nullable<int> _NumberOfPages;
-		
-		private System.Nullable<int> _PrintYear;
-		
-		private string _Publisher;
-		
 		private string _Author;
 		
-		public search_books_titlesResult()
+		public search_number_of_book_pagesResult()
 		{
 		}
 		
