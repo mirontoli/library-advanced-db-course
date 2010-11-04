4da469a6aa855712be912655c4d19007f97176ad
mirontoli <mirontoli@gmail.com>
2010-11-04 14:51:10 +0000

    lagt till sorteringsfunktionalitet i ListView Binding

--- Biblan/Biblan/Views/BindingSpike.xaml.cs
+++ Biblan/Biblan/Views/BindingSpike.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Biblan.Model;
 using Biblan.Controller;
+using System.ComponentModel;
 
 namespace Biblan.Views
 {
@@ -20,6 +21,9 @@ namespace Biblan.Views
     /// </summary>
     public partial class BindingSpike : Window
     {
+        GridViewColumnHeader _lastHeaderClicked = null;
+        ListSortDirection _lastDirection = ListSortDirection.Ascending;
+
         private Controller.Controller controller = Controller.Controller.GetInstance();
         public BindingSpike()
         {
@@ -30,6 +34,49 @@ namespace Biblan.Views
         private void TestBind()
         {
             lvBinding.ItemsSource = controller.GetAllBooks(); 
+        }
+        // inspired by:
+        // http://msdn.microsoft.com/en-us/library/ms745786.aspx
+        private void GridViewColumnHeaderClickedHandler(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader headerClicked = e.OriginalSource as GridViewColumnHeader;
+            ListSortDirection direction;
+            if (headerClicked.Role != GridViewColumnHeaderRole.Padding)
+            {
+                if (headerClicked != _lastHeaderClicked)
+                {
+                    direction = ListSortDirection.Ascending;
+                }
+                else
+                {
+                    if (_lastDirection == ListSortDirection.Ascending)
+                    {
+                        direction = ListSortDirection.Descending;
+                    }
+                    else
+                    {
+                        direction = ListSortDirection.Ascending;
+                    }
+                }
+
+                string header = headerClicked.Column.Header as string;
+                Sort(header, direction);
+
+
+                _lastHeaderClicked = headerClicked;
+                _lastDirection = direction;
+            }
+
+        }
+        private void Sort(string sortBy, ListSortDirection direction)
+        {
+            ICollectionView dataView =
+              CollectionViewSource.GetDefaultView(lvBinding.ItemsSource);
+
+            dataView.SortDescriptions.Clear();
+            SortDescription sd = new SortDescription(sortBy, direction);
+            dataView.SortDescriptions.Add(sd);
+            dataView.Refresh();
         }
     }
 }
