0df5d7497450d00c8437339a9d57c07cb6394bf2
mirontoli <mirontoli@gmail.com>
2010-11-04 11:50:41 +0000

    Städat i MainWindow

--- Biblan/Biblan/Controller/Controller.cs
+++ Biblan/Biblan/Controller/Controller.cs
@@ -7,5 +7,51 @@ namespace Biblan.Controller
 {
     public class Controller
     {
+        #region gamla tester
+        //private void AddBook()
+        //{
+        //    bibliotekDataContext dataContext = new bibliotekDataContext();
+        //    dataContext.usp_add_book("0316769533", "The Catcher in the Rye", 276, 1951, "Little, Brown and Company");
+        //}
+        //private void AddCustomer()
+        //{
+        //    bibliotekDataContext dataContext = new bibliotekDataContext();
+        //    dataContext.usp_add_customer(0001, "Nils Nilsson", "Sveagatan 31 A", "040-123456");
+        //}
+        //private void AddCopy()
+        //{
+        //    bibliotekDataContext dataContext = new bibliotekDataContext();
+        //    dataContext.usp_add_copy("0316769533", 0001);
+        //}
+        //private void GetBooksWithMoreThan500Pages()
+        //{
+        //    string output = "";
+        //    bibliotekDataContext dataContext = new bibliotekDataContext();
+        //    var query = from c in dataContext.search_number_of_book_pages()
+        //                select new Book { Title = c.Title, NumberOfPages = (int) c.NumberOfPages };
+
+        //    List<Book> books = query.ToList();
+
+        //    foreach (Book book in books)
+        //    {
+        //        output += book.Title.Substring(0,7) + " - " + book.NumberOfPages + "\r\n";            
+        //    }
+        //    txtBlockTest.Text = output;        
+        //}
+
+        //private void GetBooksOlderThan15Years()
+        //{
+        //    string output = "";
+        //    bibliotekDataContext dataContext = new bibliotekDataContext();
+        //    var query = from c in dataContext.search_book_print_year()
+        //                select new { c.Title, c.PrintYear };
+
+        //    foreach (var book in query)
+        //    {
+        //        output += book.Title + " - " + book.PrintYear + "\r\n";
+        //    }
+        //    txtBlockTest.Text = output;
+        //}
+        #endregion
     }
 }
--- Biblan/Biblan/Views/MainWindow.xaml.cs
+++ Biblan/Biblan/Views/MainWindow.xaml.cs
@@ -23,78 +23,16 @@ namespace Biblan.Views
         public MainWindow()
         {
             InitializeComponent();
-            //TestConnection();
-            //GetBooksWithMoreThan500Pages();
-            GetBooksOlderThan15Years();
-            //AddBook();
-            //AddCustomer();
-            //AddCopy();
         }
 
-        private void TestConnection()
-        {
-            string output = "";
-            bibliotekDataContext dataContext = new bibliotekDataContext();
-            var query = from c in dataContext.search_books_titles("ma")
-                        select new { c.ISBN, c.Title };
 
-            foreach (var book in query)
-            {
-                output += book.ISBN + " - " + book.Title + "\r\n";
-            }
-            txtBlockTest.Text = output;
-        }
-
-        private void GetBooksWithMoreThan500Pages()
-        {
-            string output = "";
-            bibliotekDataContext dataContext = new bibliotekDataContext();
-            var query = from c in dataContext.search_number_of_book_pages()
-                        select new Book { Title = c.Title, NumberOfPages = (int) c.NumberOfPages };
-
-            List<Book> books = query.ToList();
-
-            foreach (Book book in books)
-            {
-                output += book.Title.Substring(0,7) + " - " + book.NumberOfPages + "\r\n";            
-            }
-            txtBlockTest.Text = output;        
-        }
-
-        private void GetBooksOlderThan15Years()
-        {
-            string output = "";
-            bibliotekDataContext dataContext = new bibliotekDataContext();
-            var query = from c in dataContext.search_book_print_year()
-                        select new { c.Title, c.PrintYear };
-
-            foreach (var book in query)
-            {
-                output += book.Title + " - " + book.PrintYear + "\r\n";
-            }
-            txtBlockTest.Text = output;
-        }
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
-        private void AddBook()
-        {
-            bibliotekDataContext dataContext = new bibliotekDataContext();
-            dataContext.usp_add_book("0316769533", "The Catcher in the Rye", 276, 1951, "Little, Brown and Company");
-        }
-        private void AddCustomer()
-        {
-            bibliotekDataContext dataContext = new bibliotekDataContext();
-            dataContext.usp_add_customer(0001, "Nils Nilsson", "Sveagatan 31 A", "040-123456");
-        }
-        private void AddCopy()
-        {
-            bibliotekDataContext dataContext = new bibliotekDataContext();
-            dataContext.usp_add_copy("0316769533", 0001);
-        }
+
 
         private void btnSpike_Click(object sender, RoutedEventArgs e)
         {
