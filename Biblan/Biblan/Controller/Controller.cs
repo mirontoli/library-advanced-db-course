2f68949361421875926f3a55c7970e3d0e0f5448
mirontoli <mirontoli@gmail.com>
2010-11-08 22:29:20 +0000

    raderat "gamla tester"

--- Biblan/Biblan/Controller/Controller.cs
+++ Biblan/Biblan/Controller/Controller.cs
@@ -163,67 +163,6 @@ namespace Biblan.Controller
             return customers;
         }
 
-        #region gamla tester
-        //private void AddBook()
-        //{
-        //    bibliotekDataContext dataContext = new bibliotekDataContext();
-        //    dataContext.usp_add_book("0316769533", "The Catcher in the Rye", 276, 1951, "Little, Brown and Company");
-        //}
-        //private void AddCustomer()
-        //{
-        //    bibliotekDataContext dataContext = new bibliotekDataContext();
-        //    dataContext.usp_add_customer(0001, "Nils Nilsson", "Sveagatan 31 A", "040-123456");
-        //}
-        //private void AddCopy()
-        //{
-            //bibliotekDataContext dataContext = new bibliotekDataContext();
-            //dataContext.usp_add_copy("0316769533", 1);
-        //}
-        //private void GetBooksWithMoreThan500Pages()
-        //{
-        //    string output = "";
-        //    bibliotekDataContext dataContext = new bibliotekDataContext();
-        //    var query = from c in dataContext.search_number_of_book_pages()
-        //                select new Book { Title = c.Title, NumberOfPages = (int) c.NumberOfPages };
-
-        //    List<Book> books = query.ToList();
-
-        //    foreach (Book book in books)
-        //    {
-        //        output += book.Title.Substring(0,7) + " - " + book.NumberOfPages + "\r\n";            
-        //    }
-        //    txtBlockTest.Text = output;        
-        //}
-        //private void GetBooksThatHasATitleThatBeginsWithAnA()
-        //{
-        //    string output = "";
-        //    bibliotekDataContext dataContext = new bibliotekDataContext();
-        //    var query = from c in dataContext.search_books_with_a_title_that_begins_with_A()
-        //                select new Book { Title = c.Title, NumberOfPages = (int) c.NumberOfPages, Author = c.Author };
-
-        //    List<Book> books = query.ToList();
-
-        //    foreach (Book book in books)
-        //    {
-        //        output += book.Title.Substring(0,7) + " - " + book.NumberOfPages + " - " + book.Author "\r\n";            
-        //    }
-        //    txtBlockTest.Text = output;        
-        //}
-        //private void GetBooksOlderThan15Years()
-        //{
-        //    string output = "";
-        //    bibliotekDataContext dataContext = new bibliotekDataContext();
-        //    var query = from c in dataContext.search_book_print_year()
-        //                select new { c.Title, c.PrintYear };
-
-        //    foreach (var book in query)
-        //    {
-        //        output += book.Title + " - " + book.PrintYear + "\r\n";
-        //    }
-        //    txtBlockTest.Text = output;
-        //}
-        #endregion
-
         public void ShowSmallExtrasWindow()
         {
             SmallExtras sx = new SmallExtras();
